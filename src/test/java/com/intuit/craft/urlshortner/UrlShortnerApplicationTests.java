1b3a562ef97ee93f2d9d02a07589462301ab8433
Ashish5380 <sridharswain@Sridhars-MacBook-Air.local>
2024-05-03 01:06:27 +0530

    Inital commit

---
+++ src/test/java/com/intuit/craft/urlshortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.craft.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
